e6a0d628aa37013e320df7c57c15db073f887b73
UEFI-code <zhuoshizhang@hotmail.com>
2024-02-24 13:05:35 +0800

    Refactor debugger trace functions in dbg2.py

--- dbg2.py
+++ dbg2.py
@@ -4,25 +4,10 @@ class myCuteDebugger:
     def __init__(self):
         self.activated_debugger = False
         self.script_to_debug = None
-
-    def trace_calls(self, frame, event, arg):
-        if event != 'call':
-            print(f'Event is not call: {event}!')
-            return
-        if not self.activated_debugger:
-            print('Mushi this call...Game is not started yet!')
-            return self.trace_lines
-        co = frame.f_code
-        filename = co.co_filename
-        func_name = co.co_name
-        line_no = frame.f_lineno
-        print(f"Call to {func_name} at line {line_no} of {filename}")
-        input("Press Enter to continue")
-        return self.trace_lines
     
-    def trace_lines(self, frame, event, arg):
-        if event != 'line':
-            print(f'Event is not line: {event}!')
+    def trace_callback(self, frame, event, arg):
+        if event == 'return':
+            print('Event is return')
             return
         co = frame.f_code
         filename = co.co_filename
@@ -37,15 +22,15 @@ class myCuteDebugger:
         
         if not self.activated_debugger:
             print('Mushi this line...Game is not started yet!')
-            return self.trace_lines
-        #print(f"Executing line {line_no} of {filename} in {func_name}. Locals: {locals_dict}, Globals: {globals_dict}")
-        print(f"Executing line {line_no} of {filename} in {func_name}")
+            return self.trace_callback
+        
+        print(f"Executing line {line_no} of {filename} in {func_name}, Event: {event}")
         input("Press Enter to continue")
-        return self.trace_lines
+        return self.trace_callback
     
     def start_debugger(self, script_filename):
         self.script_to_debug = script_filename
-        sys.settrace(self.trace_calls)
+        sys.settrace(self.trace_callback)
         with open(self.script_to_debug) as f:
             code = compile(f.read(), self.script_to_debug, 'exec')
             exec(code, {}, {})
