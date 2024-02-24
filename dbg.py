8068587ce852d52789f2f175b7ab9e2c9d6972cf
UEFI-code <zhuoshizhang@hotmail.com>
2024-02-24 12:15:46 +0800

    First Upload

---
+++ dbg.py
@@ -0,0 +1,39 @@
+import sys
+
+def trace_calls(frame, event, arg):
+    if event != 'call':
+        return
+    co = frame.f_code
+    func_name = co.co_name
+    line_no = frame.f_lineno
+    filename = co.co_filename
+    print(f"Call to {func_name} at line {line_no} of {filename}")
+    input("Press Enter to continue")
+    return trace_lines
+
+def trace_lines(frame, event, arg):
+    if event != 'line':
+        return
+    co = frame.f_code
+    func_name = co.co_name
+    line_no = frame.f_lineno
+    filename = co.co_filename
+    locals_dict = frame.f_locals
+    globals_dict = frame.f_globals
+    print(f"Executing line {line_no} of {filename} in {func_name}. Locals: {locals_dict}, Globals: {globals_dict}")
+    input("Press Enter to continue")
+    return trace_lines
+
+def start_debugger(script):
+    sys.settrace(trace_calls)
+    with open(script) as f:
+        code = compile(f.read(), script, 'exec')
+        exec(code, {}, {})
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python debugger.py <script_to_debug.py>")
+        sys.exit(1)
+    script_to_debug = sys.argv[1]
+    start_debugger(script_to_debug)
+
